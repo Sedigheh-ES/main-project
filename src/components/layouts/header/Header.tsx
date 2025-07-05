4db3507d79a90cd04d54c5293bb0d95210a4c127
Sedi <sedi@gmail.com>
2025-07-05 20:45:10 +0330

    ImageView component added

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,6 +1,13 @@
+import { ImageView } from "@/components/common";
 
 export  function Header() {
   return (
-    <div>Header</div>
+    <header className="mb-[33px]">
+      <div className=" container flex items-center justify-between">
+        <ImageView src={"/assets/images/Logo.png"} alt={"Logo"} width={125} height={75} />
+
+      </div>
+
+    </header>
   )
 }

be12312d616014f8ad29a9b1b41454e7063246b3
Sedi <sedi@gmail.com>
2025-07-06 20:46:23 +0330

    icons fixed

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,5 +1,7 @@
 import { ImageView, Logo } from "@/components/common";
 import { SearchForm } from "./searchform";
+import Link from "next/link";
+
 
 export  function Header() {
   return (
@@ -8,9 +10,11 @@ export  function Header() {
          <Logo/>
         <SearchForm/>
 
-        <ul className="flex gap-2">
-          <li>account</li>
-          <li>login</li>
+        <ul className="flex gap-2 cursor-pointer">
+          <Link href="#"><li className={"icon-user text-[24px]"}> </li></Link>
+          <div className="text-medium text-gray-500" >Account</div>
+          <Link href="#"><li className={"icon-shopping-cart text-[24px]"} ></li> </Link>
+           <div className="text-medium text-gray-500" >Card</div>
         </ul>
 
       </div>
--- src/components/layouts/header/searchform/SearchForm.tsx
+++ src/components/layouts/header/searchform/SearchForm.tsx
@@ -4,7 +4,7 @@ import { IconBox } from "@/components/common";
 export function SearchForm() {
     //TODO should implement Form
     return (
-        <div className="border-2 border-green-200 rounded-lg max-w-[700px] w-full mx-[15px] px-[15px] hidden lg:inline-block">
+        <div className="border-2 border-[#3BB77E] rounded-lg max-w-[700px] w-full mx-[15px] px-[15px] hidden lg:inline-block">
             <form name="search-form" action="#" className="flex items-center">
                 <input type="text" name="search-text" placeholder="search for items"
                     className="text-sm text-gray-400 w-full py-[15px] focus:outline-none"

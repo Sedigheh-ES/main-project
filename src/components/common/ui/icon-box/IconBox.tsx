15dbf91b39c12d3f396e3cc0b7da8e17d9311021
Sedi <sedi@gmail.com>
2025-07-07 00:38:32 +0330

    badge added

--- src/components/common/ui/icon-box/IconBox.tsx
+++ src/components/common/ui/icon-box/IconBox.tsx
@@ -1,9 +1,32 @@
+import Link from "next/link";
+
 interface Props{
     icon: string;
-    size?: number;
+  size?: number;
+  link?: string;
+  title?: string;
+  hideTitleOnMobile?: boolean;
+  badge?:number
 }
-export  function IconBox({icon,size=22}:Props) {
+export function IconBox({icon,size=22,link,title,hideTitleOnMobile=false,badge=0}:Props) {
   return (
-    <i className={`${icon} text-[${size}px]`}></i>
+    <>
+      {
+        badge ?
+         <div className="relative">
+            <span className="absolute bg-green-600 rounded-full flex justify-center items-center text-white px-3 py-1 -top-[10px] -right-[10px]  w-[20px] h-[20px]">{badge}</span>
+             <Link href={link ?? '#'} ><i className={`${icon} text-[${size}px]`}></i> </Link>
+          </div>
+          :
+
+        <Link href={link ?? '#'} > <i className={`${icon} text-[${size}px]`}></i></Link>
+
+      }
+
+            {title && <div className={`${hideTitleOnMobile ? 'hidden xl:inline-block' : 'inline-block'} text-medium text-gray-500`} > {title}</div>}
+
+    </>
+    
+   
   )
 }
--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,20 +1,23 @@
-import { ImageView, Logo } from "@/components/common";
+import { IconBox, ImageView, Logo } from "@/components/common";
 import { SearchForm } from "./searchform";
 import Link from "next/link";
 
 
 export  function Header() {
   return (
-    <header className="mb-[33px]">
+    <header className="flex items-center justify-center mb-[33px]">
       <div className=" container flex items-center justify-between py-4">
          <Logo/>
         <SearchForm/>
 
-        <ul className="flex gap-2 cursor-pointer">
-          <Link href="#"><li className={"icon-user text-[24px]"}> </li></Link>
-          <div className="text-medium text-gray-500" >Account</div>
-          <Link href="#"><li className={"icon-shopping-cart text-[24px]"} ></li> </Link>
-           <div className="text-medium text-gray-500" >Card</div>
+        <ul className="hidden lg:flex gap-5 cursor-pointer">
+          <li className="flex gap-2 cursor-pointer ">
+                      <IconBox icon={'icon-user'} size={24} title={'Account'} link={'#'} hideTitleOnMobile={true} />
+          </li>
+         
+          <li className="flex gap-2 cursor-pointer ">
+          <IconBox icon={'icon-shopping-cart'} size={24} title={'Card'} link={'#'}  hideTitleOnMobile={true} badge={2} />         
+          </li>
         </ul>
 
       </div>

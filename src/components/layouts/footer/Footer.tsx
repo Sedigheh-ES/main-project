c45a987324954aa39b55554975ea17a0657bf105
Sedi <sedi@gmail.com>
2025-07-14 20:16:20 +0330

    some responsive fixed on header and footer

--- src/components/layouts/footer/Footer.tsx
+++ src/components/layouts/footer/Footer.tsx
@@ -11,15 +11,16 @@ export function Footer() {
 
         <div className="rounded-[6px] p-10 md:rounded-[14px] lg:rounded-[30px] bg-hero-pattern bg-[rgba(59,128,126,.2)] bg-opacity-20 bg-cover bg-top bg-no-repeat flex justify-between items-center my-[38px] relative">
           <div className="min-h-[160px] pl-3 pt-3 sm:pl-4 sm:pt4 md:pl-6 md:pt-6 lg:pl-10 lg:py-10 xl:pl-14 xl:py-14 2xl:py-[72px] 2xl:pl-[72px]">
-            <h2 className="max-w-[60%] font-quickSand text-2xl sm:text-3xl md:text-2xl lg:text-5xl tracking-[-0.04%] text-[#1C4037]">Stay home & get your daily needs from our shop</h2>
+            <h2 className="max-w-[50%] font-quickSand text-2xl sm:text-3xl md:text-2xl lg:text-5xl tracking-[-0.04%] text-[#1C4037]">Stay home & get your daily needs from our shop</h2>
             <div className="font-lato text-2xl tracking-[-0.04%] text-[#2B3D34] mt-8 hidden lg:block">Start Your Daily Sopping with Nest Mart</div>
           </div>
-          <ImageView src={"/assets/images/Screenshot (76).png"} width={587} height={331} classname={"absolute bottom-0 right-0 sm:h-full h-3/4"} alt={"Footer Banner"} />
+          <ImageView src={"/assets/images/Screenshot (76).png"} width={587} height={331} classname={"absolute bottom-0 right-0 sm:h-full w-[50%] hidden lg:flex "} alt={"Footer Banner"} />
+          <ImageView src={"/assets/images/fresh-apple.png"} width={587} height={331} classname={"absolute bottom-0 right-0 sm:h-full w-[50%] flex lg:hiddeen "} alt={"Footer Banner"} />
         </div>
       </div>
 
       {/* Footer Bottom */}
-      <div className='flex flex-col md:flex-row justify-between  gap-4.5 '>
+      <div className='flex flex-col md:flex-row justify-between gap-4.5'>
         <div className='max-w-[346px]'>
           <Logo />
           <div className='font-medium font-quickSand mt-4 lg:mt-6'>Pellentesque posuere orci lobortis</div>
@@ -43,9 +44,11 @@ export function Footer() {
             <IconBox icon={'icon-time-fast'} size={16} />
             <div className='font-medium font-quickSand'><span className='font-bold'>Hours:</span> 10:00 - 18:00, Mon - Sat  </div>
           </div>
+           
+
         </div>
 
-        <div>
+        <div >
           <div className='font-quickSand font-bold text-3xl text-[#253D4E]'>Company</div>
           <ul className='hidden md:flex flex-col gap-2.5'>
             <li className='font-quickSand text-base text-[#253D4E] mt-4 lg:mt-6'><Link href={'#'} />About Us</li>
@@ -59,7 +62,7 @@ export function Footer() {
 
         </div>
 
-        <div>
+        <div> 
           <div className='font-quickSand font-bold text-3xl text-[#253D4E]'>Information</div>
           <ul className='hidden md:flex flex-col gap-2.5'>
             <li className='font-quickSand text-base text-[#253D4E] mt-4 lg:mt-6'><Link href={'#'} />Search Terms</li>
@@ -72,7 +75,7 @@ export function Footer() {
 
         </div>
 
-        <div className='className="pt-4 md:pt-0"'>
+        <div>
           <div className='font-quickSand font-bold text-3xl text-[#253D4E]'>App & Paymnent</div>
           <div className='text-sm font-quickSand mt-4 lg:mt-6 text-[#7E7E7E]'>Install NetMart App from App Store or Google Play</div>
           <div className='flex items-center justify-center gap-6 pt-4 lg:pt-5'>

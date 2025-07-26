5c06f30cfdd19d1774daa003deaa97b9d137de1a
Sedi <sedi@gmail.com>
2025-07-26 23:49:55 +0330

    BrowsCategory Menu clickHandler

--- src/api/config/ApiClient.ts
+++ src/api/config/ApiClient.ts
@@ -1,34 +1,34 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-
 const apiClient = axios.create({
-    baseURL: 'https://nest.navaxcollege.com/api',
-    timeout:120000
-})
-
-apiClient.interceptors.response.use(function (response) {
-    return response.data;
-}, function (error) {
-    if (error.response) {
-        if (error.response.status === 404) {
-            toast.error('منابع درخواستی وجود ندارد.');
-        } else if (error.response.status === 400) {
-            toast.error('پارامترهای ارسالی صحیح نمیباشد.');
-        } else if (error.response.status === 401) {
-            toast.error('لطفا وارد شوید.');
-        } else if (error.response.status === 403) {
-            toast.error('شما دسترسی به این منابع ندارید.');
-
-        } else {
-            toast.error('خطایی رخ داده است.');
-        }
-        
-    } else if (error.request) {
-        toast.error('سرور در دسترس نمی باشد');
-    } else {
-        toast.error('خطای نامعلوم');
-    }
-    return Promise.reject(error)
+  baseURL: "https://nest.navaxcollege.com/api",
+  timeout: 120000,
 });
+
+apiClient.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    if (error.response) {
+      if (error.response.status === 404) {
+        toast.error("منابع درخواستی وجود ندارد.");
+      } else if (error.response.status === 400) {
+        toast.error("پارامترهای ارسالی صحیح نمیباشد.");
+      } else if (error.response.status === 401) {
+        toast.error("لطفا وارد شوید.");
+      } else if (error.response.status === 403) {
+        toast.error("شما دسترسی به این منابع ندارید.");
+      } else {
+        toast.error("خطایی رخ داده است.");
+      }
+    } else if (error.request) {
+      toast.error("سرور در دسترس نمی باشد");
+    } else {
+      toast.error("خطای نامعلوم");
+    }
+    return Promise.reject(error);
+  }
+);
 export default apiClient;
--- src/components/layouts/header/menu/Menu.tsx
+++ src/components/layouts/header/menu/Menu.tsx
@@ -1,81 +1,128 @@
-
 import { IconBox } from "@/components/common";
 import { useMenu } from "@/hooks/use-menu";
 import { EntityType, MenuItemType, MenuType, PopulateType } from "@/types";
 import Link from "next/link";
-import { useState } from "react";
-
-
-
-
+import { useEffect, useState } from "react";
 
 export function Menu() {
-    const [showCategoryMenu, setShowCategoryMenu] = useState<boolean>(false);
+  const [showCategoryMenu, setShowCategoryMenu] = useState<boolean>(false);
 
-    const { data: mainMenuItems } = useMenu({ position: 'main_menu' });
-    const { data: categoryMenuItems } = useMenu({ position: 'brows-category' });
+  const { data: mainMenuItems } = useMenu({ position: "main_menu" });
+  const { data: categoryMenuItems } = useMenu({ position: "brows-category" });
 
-    const categoryMenuClickHandler = () => {
-        setShowCategoryMenu((prevState) => !prevState);
-    }
-
-    const categoryBodyClickHandler = (e) => {
+    const categoryMenuClickHandler = (e) => {
         e.stopPropagation();
-    }
+    setShowCategoryMenu((prevState) => !prevState);
+  };
 
-    return (
-        <>
-            {/* Brows all category */}
-            <div id="all_categpries" onClick={categoryMenuClickHandler} className="hidden relative w-[250px] h-[50px] bg-[#3BB77E] rounded-[5px] border border-gray-200 lg:flex justify-center items-center gap-2 cursor-pointer">
-                <span className={"flex items-center justify-center gap-x-2"}>
-                    <IconBox linkClassName={"text-white font-bold"} icon={'icon-apps'} size={24} link={'#'} hideTitleOnMobile={true} title={"Browse All Categories"} />
-                </span>
-                <span className={"flex items-center justify-center gap-x-1"}>
-                    <IconBox linkClassName={"text-white"} icon={'icon-angle-small-down'} size={24} link={'#'} hideTitleOnMobile={false} titleClassName={"text-meduim text-white"} />
-                </span>
+    const categoryBodyClickHandler = () => {
+     
+ }
 
-                {/* All Categories Menu */}
-                <div id="all_categories_box" onClick={categoryBodyClickHandler} className={`${showCategoryMenu ? 'flex' : 'hidden'} bg-white w-[500px] h-auto border absolute left-0 top-16 rounded-[10px] border-[#BCE3C9] p-2.5 z-10`}>
-                    <div id="all_cat_inner_box" className="flex flex-wrap justify-between gap-y-[15px]">
-                        {
-                            categoryMenuItems &&
-                            categoryMenuItems.data.map((item: EntityType<MenuItemType>, index: number) => {
-                                return <IconBox key={index} link={item.attributes.link} icon={item.attributes.icon_name} size={30} title={item.attributes.title} titleClassName={"text-[#253D4E] text-sm font-bold"}
-                                    linkClassName={"flex items-center gap-3.5 rounded-[5px] lg:border-[1px] lg:border-gray-300 py-2.5  basis-full lg:basis-[calc(50%-8px)] justify-start pl-4 lg:hover:border-green-300 cursor-pointer"} path={item.attributes.icon_path} />
-                            })
-                        }
-                    </div>
+    useEffect(() => {
+        const clickHandler= ()=>{ setShowCategoryMenu(false)}
+        document.addEventListener('click', (clickHandler) => {
+            setShowCategoryMenu(false)
+        })
+        return () => {
+            document.removeEventListener('click',clickHandler)
+        }
+    }, []);
 
-                </div>
-            </div>
+  return (
+    <>
+          {/* Brows all category */}
+          
+          <div className={'relative'} onClick={categoryMenuClickHandler}>
+          <div className="hidden relative w-[250px] h-[50px] bg-[#3BB77E] rounded-[5px] border border-gray-200 lg:flex justify-center items-center gap-2 cursor-pointer ">
+                 
+          <IconBox
+            linkClassName={"text-white font-bold"}
+            icon={"icon-apps"}
+            size={24}
+            link={"#"}
+            hideTitleOnMobile={true}
+            title={"Browse All Categories"}
+          />
+       
+       
+          <IconBox
+            linkClassName={"text-white"}
+            icon={"icon-angle-small-down"}
+            size={24}
+            link={"#"}
+            hideTitleOnMobile={false}
+            titleClassName={"text-meduim text-white"}
+          />
+       
+              </div>
+              {/* All Categories Menu*/}
+        <div
+         
+          onClick={categoryBodyClickHandler}
+          className={`${
+            showCategoryMenu ? "flex" : "hidden"
+          } bg-white w-[500px] h-auto border absolute left-0 top-16 rounded-[10px] border-[#BCE3C9] p-2.5 z-10`}
+             >
+          <div
+           
+            className="flex flex-wrap justify-between gap-y-[15px]"
+             >
+            {categoryMenuItems &&
+              categoryMenuItems.data.map(
+                (item: EntityType<MenuItemType>, index: number) => {
+                  return (
+                    <IconBox
+                      key={index}
+                      link={item.attributes.link}
+                      icon={item.attributes.icon_name}
+                      size={30}
+                      title={item.attributes.title}
+                      titleClassName={"text-[#253D4E] text-sm font-bold"}
+                      linkClassName={
+                        "flex items-center gap-3.5 rounded-[5px] lg:border-[1px] lg:border-gray-300 py-2.5  basis-full lg:basis-[calc(50%-8px)] justify-start pl-4 lg:hover:border-green-300 cursor-pointer"
+                      }
+                      path={item.attributes.icon_path}
+                    />
+                  );
+                }
+              )}
+          </div>
+        </div>
+        </div>
+          
+    
 
-            <nav id="nav-menu">
-                <ul className="hidden xl:flex flex-col lg:flex-row items-start gap-2.5 justify-center font-bold">
-                    {
-                        mainMenuItems &&
-                        mainMenuItems.data.map((item: EntityType<MenuItemType>, index: number) => {
-                            return (
-                                <li key={index}>
-                                    {
-                                        item.attributes.icon_name ?
-                                            <IconBox link={item.attributes.link} icon={item.attributes.icon_name} title={item.attributes.title} size={24} titleClassName="ml-1" />
-                                            :
-                                            <Link href={item.attributes.link} className={"flex items-center gap-1"} > {item.attributes.title}</Link>
-                                    }
-                                </li>
-                            )
-                        })
-                    }
-
-
-                    
-
-                </ul>
-
-            </nav>
-
-
-
-        </>
-    )
+      <nav id="nav-menu">
+        <ul className="hidden xl:flex flex-col lg:flex-row items-start gap-2.5 justify-center font-bold">
+          {mainMenuItems &&
+            mainMenuItems.data.map(
+              (item: EntityType<MenuItemType>, index: number) => {
+                return (
+                  <li key={index}>
+                    {item.attributes.icon_name ? (
+                      <IconBox
+                        link={item.attributes.link}
+                        icon={item.attributes.icon_name}
+                        title={item.attributes.title}
+                        size={24}
+                        titleClassName="ml-1"
+                      />
+                    ) : (
+                      <Link
+                        href={item.attributes.link}
+                        className={"flex items-center gap-1"}
+                      >
+                        {" "}
+                        {item.attributes.title}
+                      </Link>
+                    )}
+                  </li>
+                );
+              }
+            )}
+        </ul>
+      </nav>
+    </>
+  );
 }

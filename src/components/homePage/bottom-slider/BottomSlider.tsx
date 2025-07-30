d96d5038264402a118bd7beb5074dc062d837521
Sedi <sedi@gmail.com>
2025-07-30 20:35:03 +0330

    bottom Slider inView

--- src/components/homePage/bottom-slider/BottomSlider.tsx
+++ src/components/homePage/bottom-slider/BottomSlider.tsx
@@ -1,20 +1,11 @@
 import { getAllProductsApiCall } from "@/api/Product";
-import { IconBox, ImageView } from "@/components/common";
-import ProductDealsCard from "@/components/common/product/deals-of-the-day-card/ProductDealsCard";
 import ProductverticalList from "@/components/common/product/product-vertical-list/ProductverticalList";
-import { TopRatedMock } from "@/mock/TopRatedMock";
-import { popularProducts } from "@/mock/popularProducts";
-import { RecentlyAddedMock } from "@/mock/recentlyAddedMock";
-import { TopSellingMock } from "@/mock/topSellerMock";
-import { TrendingProductsMock } from "@/mock/trendingProduct";
 import { ApiResponseType, EntityType } from "@/types";
 import { ProductType } from "@/types/api/Product";
 import { useQuery } from "@tanstack/react-query";
-import { permission } from "process";
 import { Autoplay, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-
+import { InView } from "react-intersection-observer";
 
 interface Props {
     sliderData:Array<EntityType<ProductType>>,
@@ -23,19 +14,20 @@ interface Props {
 }
 
 export default function BottomSlider({sliderData}: Props) {
-     const { data:topRateData } = useQuery<ApiResponseType<ProductType>>({ 
+     const { data:topRateData,refetch } = useQuery<ApiResponseType<ProductType>>({ 
     queryKey: [getAllProductsApiCall.name,'top_rate'],
          queryFn: () => getAllProductsApiCall(
              {
                  populate: ["thumbnail"],
                  sort: ["rate:desc"],
                  pagination: {
-                     page: 1,
-                     pageSize:3
-                 }
-             })
+                     start: 0,
+                     limit: 3               
+                 },
+             }),
+         enabled:false
     })
-    console.log("top rated",topRateData);
+  
     
 
    const { data:isTopSellingData } = useQuery<ApiResponseType<ProductType>>({ 
@@ -43,14 +35,14 @@ export default function BottomSlider({sliderData}: Props) {
   queryFn: () => getAllProductsApiCall({ populate: ["categories", "thumbnail"], filters: { is_top_selling: { $eq: true } } })
   })
 
-    console.log('top Selling', isTopSellingData);
+   
     
     const { data:isTrendingData } = useQuery<ApiResponseType<ProductType>>({ 
     queryKey: [getAllProductsApiCall.name,'trending_data'],
   queryFn: () => getAllProductsApiCall({ populate: ["categories", "thumbnail"], filters: { is_trending: { $eq: true } } })
     })
     
-    console.log('Trending', isTrendingData);
+   
 
      const { data:RecentlyAdded} = useQuery<ApiResponseType<ProductType>>({ 
     queryKey: [getAllProductsApiCall.name,'recent_data'],
@@ -67,7 +59,7 @@ export default function BottomSlider({sliderData}: Props) {
              
          )
      })
-    console.log("Recenlty",RecentlyAdded);
+    
 
     return (
         <Swiper
@@ -105,12 +97,15 @@ export default function BottomSlider({sliderData}: Props) {
               { isTrendingData && <ProductverticalList title={'Trending Products'} data={isTrendingData?.data} />}
             </SwiperSlide>
 
-            {/* <SwiperSlide>
-                <ProductverticalList title={'Recently added'} data={RecentlyAddedMock} />
-            </SwiperSlide> */}
+            <SwiperSlide>
+              { RecentlyAdded && <ProductverticalList title={'Recently added'} data={RecentlyAdded.data} />}
+            </SwiperSlide>
 
+            
             <SwiperSlide> 
-               {topRateData && <ProductverticalList title={'Top Rated'} data={topRateData.data} />}
+                <InView as="div" onChange={(inView, entry) => inView && refetch()}>
+                    {topRateData && <ProductverticalList title={'Top Rated'} data={topRateData.data} />}
+                    </InView>
             </SwiperSlide>
         </Swiper>
     );

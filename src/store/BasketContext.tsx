3a57283b6eee412771aa740142611a5df3c73f3f
Sedi <sedi@gmail.com>
2025-08-12 16:02:56 +0330

    useReducerr part done

--- src/store/BasketContext.tsx
+++ src/store/BasketContext.tsx
@@ -1,6 +1,6 @@
 import { EntityType } from "@/types";
 import { ProductType } from "@/types/api/Product";
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useReducer, useState } from "react";
 
 interface Props {
     children: ReactNode;
@@ -30,55 +30,78 @@ export const BasketContext = createContext<{
 
 });
 
+
+type Action = { type: "ADD_ITEM", product: EntityType<ProductType> }
+              | { type: "INCREASE_ITEM", productId: number }
+              | { type: "DECREASE_ITEM", productId: number }
+              | {type:"DELETE_ITEM",productId:number}
+
+const basketReducer = (currentState:ProductItem[],action:Action) => { 
+    switch (action.type) {
+        case "ADD_ITEM":
+            return [
+                ...currentState,
+                {
+            productId: action.product.id,
+            title: action.product.attributes.title,
+            img:action.product.attributes.thumbnail?.data?.attributes.url,
+            price: action.product.attributes.price,
+            quantity: 1
+                }
+            ]
+        case "INCREASE_ITEM":
+            return currentState.map((item) => {
+            if (item.productId === action.productId)
+                return { ...item, quantity: item.quantity + 1 };
+            return item;
+            })
+        case "DECREASE_ITEM": 
+     const currentProductId = currentState.find((item) => item.productId === action.productId);
+        if (currentProductId && currentProductId.quantity === 1) {
+            return currentState.filter((item) => item.productId !== action.productId);
+        } 
+            return  currentState.map((item) => {
+                if (item.productId === action.productId)
+                    return { ...item, quantity: item.quantity - 1 };
+                return item;
+            })
+        case "DELETE_ITEM":
+            return currentState.filter((item) => item.productId !== action.productId);
+        default:
+            return currentState;
+            
+       
+            
+        
+}
+};
+
 export const BasketContextProvider = (props: Props) => {
 
-    const [basketItems, setBasketItems] = useState<Array<ProductItem>>([]);
+    // const [basketItems, setBasketItems] = useState<Array<ProductItem>>([]);
+
+    const [basketItems, dispatch] = useReducer(basketReducer,[]);
+    
 
     const addItemHandler = (product: EntityType<ProductType>) => {
-        const newProduct: ProductItem = {
-            productId: product.id,
-            title: product.attributes.title,
-            img: product.attributes.thumbnail?.data?.attributes.url,
-            price: product.attributes.price,
-            quantity: 1
-        }
-        setBasketItems(prevState => [
-            ...prevState,
-            newProduct
-        ])
-
+        dispatch({ type: "ADD_ITEM", product: product });
 
     }
 
 
     const increaseItemHadler = (productId: number) => {
-        const newBasket = basketItems.map((item) => {
-            if (item.productId === productId)
-                return { ...item, quantity: item.quantity + 1 };
-            return item;
-        })
-        setBasketItems(newBasket);
+        dispatch({type:"INCREASE_ITEM",productId:productId})
     }
 
 
     const decreaseItemHandler = (productId: number) => {
-        const currentProductId = basketItems.find((item) => item.productId === productId);
-        if (currentProductId && currentProductId.quantity === 1) {
-            deleteItemHandler(productId);
-        } else {
-            const newBasket = basketItems.map((item) => {
-                if (item.productId === productId)
-                    return { ...item, quantity: item.quantity - 1 };
-                return item;
-            })
-            setBasketItems(newBasket);
-        }
+        dispatch({ type: "DECREASE_ITEM", productId: productId });
     }
 
 
+
     const deleteItemHandler = (productId: number) => {
-        const newBasket = basketItems.filter((item) => item.productId !== productId);
-        setBasketItems(newBasket);
+        dispatch({type:"DELETE_ITEM",productId:productId})
     }
 
 
